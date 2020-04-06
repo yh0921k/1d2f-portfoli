6bf4dcd4517f2333d80cf2b666b8b1e311208e3f
Moonsolid <rnreo20@gmail.com>
2020-04-06 17:31:35 +0900

    [mod] DDL, DML SQl 스크립트 및 erd 수정
    - modeling/1d2f-erd.exerd, database/portfoli.ddl 변경(테이블 및 컬럼변경)
    
    1d2f-erd.exerd 변경
    [컬럼명 변경]
     - pf_job_posting
      - minimum_education -> minimum_career
    
    postfoli.ddl 변경
    [컬럼명 변경]
     - pf_job_posting
      -minimum_education -> minimum_career
    
    - script/portfoli.sql 변경(테이블 데이터 추가)
     - pf_employment_status
     - pf_major
     - pf_job_posting
     - pf_job_posting_file
     - pf_company_required_major
     - pf_job_recommendation
     - pf_general_mem_edu
     - pf_general_mem_edu_major
     - pf_company_required_certificate

--- docs/database/script/portfoli-dml.sql
+++ docs/database/script/portfoli-dml.sql
@@ -240,3 +240,129 @@ INSERT INTO pf_interest_location(general_member_no, location_no) VALUES(5, 3);
 INSERT INTO pf_interest_location(general_member_no, location_no) VALUES(4, 4);
 
 INSERT INTO pf_interest_location(general_member_no, location_no) VALUES(5, 1);
+
+
+-- pf_employment_status 고용형태
+insert into pf_employment_status(employment_stat_no,employment_stat_name)
+  values(1,'정규직');
+insert into pf_employment_status(employment_stat_no,employment_stat_name)
+  values(2,'계약직');
+insert into pf_employment_status(employment_stat_no,employment_stat_name)
+  values(3,'인턴');
+insert into pf_employment_status(employment_stat_no,employment_stat_name)
+  values(4,'경력직');
+insert into pf_employment_status(employment_stat_no,employment_stat_name)
+  values(5,'파견직');
+
+-- pf_major 전공 데이터
+insert into pf_major(major_no, name)
+  values(1,'손영국');
+insert into pf_major(major_no, name)
+  values(2,'김용휘');
+insert into pf_major(major_no, name)
+  values(3,'전은혜');
+insert into pf_major(major_no, name)
+  values(4,'송율리아');
+insert into pf_major(major_no, name)
+  values(5,'문국대');
+  
+-- pf_job_posting 채용공고 (pf_company_member 인서트 후)
+insert into pf_job_posting (job_posting_no,company_member_no,title,content,work_place_no,
+minimum_career,view_count,posting_registration,start_dated,end_dated,job,thumbnail,
+year_salary,readable,minimum_education_no,employment_stat_no)
+  values(1,1,'프론트엔드 개발자 모집','내용1',1,1,1,'2020-1-1','2020-1-1','2020-2-2','프론트엔드','www.google.com',
+  26000000,1,1,1);  
+insert into pf_job_posting (job_posting_no,company_member_no,title,content,work_place_no,
+minimum_career,view_count,posting_registration,start_dated,end_dated,job,thumbnail,
+year_salary,readable,minimum_education_no,employment_stat_no)
+  values(2,2,'백엔드 개발자 모집','내용2',2,2,2,'2020-2-2','2020-2-2','2020-3-3','백엔드','www.google.com',
+  28000000,2,2,2);  
+insert into pf_job_posting (job_posting_no,company_member_no,title,content,work_place_no,
+minimum_career,view_count,posting_registration,start_dated,end_dated,job,thumbnail,
+year_salary,readable,minimum_education_no,employment_stat_no)
+  values(3,3,'서버 개발자 모집','내용3',3,3,3,'2020-3-3','2020-3-3','2020-4-4','서버개발','www.google.com',
+  30000000,3,3,3);  
+insert into pf_job_posting (job_posting_no,company_member_no,title,content,work_place_no,
+minimum_career,view_count,posting_registration,start_dated,end_dated,job,thumbnail,
+year_salary,readable,minimum_education_no,employment_stat_no)
+  values(4,4,'자바 개발자 모집','내용4',4,4,4,'2020-4-4','2020-4-4','2020-5-5','자바','www.google.com',
+  32000000,4,4,4);  
+insert into pf_job_posting (job_posting_no,company_member_no,title,content,work_place_no,
+minimum_career,view_count,posting_registration,start_dated,end_dated,job,thumbnail,
+year_salary,readable,minimum_education_no,employment_stat_no)
+  values(5,5,'풀스택 개발자 모집','내용5',5,5,5,'2020-5-5','2020-5-5','2020-6-6','풀스택','www.google.com',
+  34000000,5,5,5);  
+  
+-- pf_job_posting_file 채용공고첨부파일 데이터(pf_job_posting 인서트 후)
+insert into pf_job_posting_file(job_posting_file_no,job_posting_no,file_path)
+  values(1,1,'/file/path'); 
+insert into pf_job_posting_file(job_posting_file_no,job_posting_no,file_path)
+  values(2,2,'/file/path'); 
+insert into pf_job_posting_file(job_posting_file_no,job_posting_no,file_path)
+  values(3,3,'/file/path'); 
+insert into pf_job_posting_file(job_posting_file_no,job_posting_no,file_path)
+  values(4,4,'/file/path'); 
+insert into pf_job_posting_file(job_posting_file_no,job_posting_no,file_path)
+  values(5,5,'/file/path');  
+  
+-- pf_company_required_major 기업요구전공 데이터(pf_major , pf_job_posting 인서트 후) 
+insert into pf_company_required_major(major_no, job_posting_no)
+  values(1,1);
+insert into pf_company_required_major(major_no, job_posting_no)
+  values(2,2);
+insert into pf_company_required_major(major_no, job_posting_no)
+  values(3,3);
+insert into pf_company_required_major(major_no, job_posting_no)
+  values(4,4);
+insert into pf_company_required_major(major_no, job_posting_no)
+  values(5,5); 
+  
+-- pf_job_recommendation 채용추천 (pf_job_posting 인서트 후)
+insert into pf_job_recommendation (general_member_no,job_posting_no)
+  values(1,1);
+insert into pf_job_recommendation (general_member_no,job_posting_no)
+  values(2,2);
+insert into pf_job_recommendation (general_member_no,job_posting_no)
+  values(3,3);
+insert into pf_job_recommendation (general_member_no,job_posting_no)
+  values(4,4);
+insert into pf_job_recommendation (general_member_no,job_posting_no)
+  values(5,5);  
+  
+
+-- pf_general_mem_edu 일반회원학력 데이터(pf_final_education 인서트 후)
+insert into pf_general_mem_edu(general_member_no,education_no,school_name)
+  values(1,1,'서울대학교');
+insert into pf_general_mem_edu(general_member_no,education_no,school_name)
+  values(1,1,'고려대학교');
+insert into pf_general_mem_edu(general_member_no,education_no,school_name)
+  values(1,1,'연세대학교');
+insert into pf_general_mem_edu(general_member_no,education_no,school_name)
+  values(1,1,'세종대학교');
+insert into pf_general_mem_edu(general_member_no,education_no,school_name)
+  values(1,1,'한양대학교');
+  
+-- pf_general_mem_edu_major 일반회원학력전공(pf_general_mem_edu 인서트 후)
+insert into pf_general_mem_edu_major (general_member_no,education_no,major_no)
+  values(1,1,1);
+insert into pf_general_mem_edu_major (general_member_no,education_no,major_no)
+  values(2,2,2);
+insert into pf_general_mem_edu_major (general_member_no,education_no,major_no)
+  values(3,3,3);
+insert into pf_general_mem_edu_major (general_member_no,education_no,major_no)
+  values(4,4,4);
+insert into pf_general_mem_edu_major (general_member_no,education_no,major_no)
+  values(5,5,5);
+  
+-- pf_company_required_certificate 기업요구자격증 (pf_certificate 인서트 후)
+insert into pf_company_required_certificate  (certificate_no,job_posting_no)
+  values(1,1);
+insert into pf_company_required_certificate  (certificate_no,job_posting_no)
+  values(2,2);
+insert into pf_company_required_certificate  (certificate_no,job_posting_no)
+  values(3,3);
+insert into pf_company_required_certificate  (certificate_no,job_posting_no)
+  values(4,4);
+insert into pf_company_required_certificate  (certificate_no,job_posting_no)
+  values(5,5);  
+ 
