29cd7942087e99b826445a61f7e200edd2ea97e8
Yonghwi Kim <yh0921k@gmail.com>
2020-04-21 15:09:11 +0900

    [add] Admin 관련 디스패처 관련 설정 추가
    - com.portfoli.admin에 존재하는 컴포넌트 관리를 위한 AppWebConfig 추가
    - 위의 설정을 기반한 디스패처 추가

---
+++ src/main/java/com/portfoli/admin/AdminWebApplicationInitializer.java
@@ -0,0 +1,36 @@
+package com.portfoli.admin;
+
+import javax.servlet.Filter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AdminWebApplicationInitializer
+    extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return null;
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class<?>[] {AdminWebConfig.class};
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] {"/admin/*"};
+  }
+
+  @Override
+  protected String getServletName() {
+    return "admin";
+  }
+
+  @Override
+  protected Filter[] getServletFilters() {
+    return new Filter[] {new CharacterEncodingFilter("UTF-8")};
+  }
+}
+
+
