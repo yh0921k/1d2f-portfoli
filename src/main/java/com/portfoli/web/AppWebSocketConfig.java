3147cc2e5304773513be1cbd1e1b7f812de00a6e
jeoneunhye <eunhdev@gmail.com>
2020-04-28 15:32:50 +0900

    [add] 헤더에 사용자 메시지 알림 카운트 적용
    src/main/resources/com/portfoli/mapper/MessageMapper.xml 수정
    - 읽지 않은 메시지를 카운트하는 sql문 countNotRead 추가
    
    src/main/java/com/portfoli/dao/MessageDao.java 수정
    - countNotRead(int) 메서드 선언
    
    build.gradle 수정
    - Dependency Spring Web Socket 라이브러리 추가
    
    src/main/java/com/portfoli/web/AppWebSocketConfig.java 추가
    - WebSocket Handler를 관리할 Config 추가
    
    src/main/java/com/portfoli/web/MessageWebSocketHandler.java 추가
    - 읽지 않은 메시지를 실시간으로 알림받는 핸들러 추가
    
    src/main/webapp/WEB-INF/jsp/header.jsp 수정
    - 헤더에 메시지 알림 카운트 적용

---
+++ src/main/java/com/portfoli/web/AppWebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.portfoli.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class AppWebSocketConfig implements WebSocketConfigurer {
+  @Autowired
+  MessageWebSocketHandler messageWebSocketHandler;
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(messageWebSocketHandler, "/message/alert")
+        .addInterceptors(new HttpSessionHandshakeInterceptor()).setAllowedOrigins("*");
+  }
+}
---
+++ src/main/java/com/portfoli/web/MessageWebSocketHandler.java
@@ -0,0 +1,52 @@
+package com.portfoli.web;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import com.portfoli.dao.MessageDao;
+import com.portfoli.domain.Member;
+
+@Component
+public class MessageWebSocketHandler extends TextWebSocketHandler {
+  private Map<String, WebSocketSession> userSessions = new ConcurrentHashMap<>();
+
+  @Autowired
+  MessageDao messageDao;
+
+  @Override
+  public void afterConnectionClosed(WebSocketSession session, CloseStatus status)
+      throws Exception {
+    super.afterConnectionClosed(session, status);
+  }
+
+  @Override
+  public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    userSessions.put(getUserId(session), session);
+  }
+
+  private String getUserId(WebSocketSession session) {
+    Map<String, Object> httpSession = session.getAttributes();
+    
+    Member loginUser = (Member) httpSession.get("loginUser");
+    if (loginUser != null) {
+      return String.valueOf(loginUser.getNumber());
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    Map<String, Object> httpSession = session.getAttributes();
+    Member loginUser = (Member) httpSession.get("loginUser");
+
+    userSessions.get(getUserId(session))
+        .sendMessage(
+            new TextMessage(String.valueOf(messageDao.countNotRead(loginUser.getNumber()))));
+  }
+}
