f8ddeb639b47b3e83e514c34e0145a9b336c90bc
Yonghwi Kim <yh0921k@gmail.com>
2020-05-14 15:22:42 +0900

    [mod] 출력을 위한 AdminLogController 변경
    - 현재 더보기 기능을 위한 변수는 주석처리
    - 단순히 10 개의 로그를 읽어 jsp로 전달

--- src/main/java/com/portfoli/admin/AdminLogController.java
+++ src/main/java/com/portfoli/admin/AdminLogController.java
@@ -3,14 +3,13 @@ package com.portfoli.admin;
 import java.sql.Date;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import com.portfoli.domain.Admin;
+import com.portfoli.service.AdminLogService;
 
 @Controller("log")
 public class AdminLogController {
@@ -20,35 +19,39 @@ public class AdminLogController {
   @Autowired
   ServletContext servletContext;
 
-  //@Autowired
-  //LogService LogService;
+  @Autowired
+  AdminLogService adminLogService;
 
   public AdminLogController() {
     logger.info("AdminLogController 객체 생성!");
   }
 
   @GetMapping("contentsLog")
-  public void contentsLog(HttpSession session) throws Exception {
+  public void contentsLog(HttpServletRequest request, Model model) throws Exception {
     System.out.println(new Date(System.currentTimeMillis()).toString());
+    String filepath = System.getProperty("catalina.base") + "/logs/portfoli.log";
+    // int startLine = (int) (request.getAttribute("startLine"));
+    // int moreLine = (int) (request.getAttribute("moreLine"));
+    model.addAttribute("loglist", adminLogService.get(filepath, 0, 10));
   }
 
-//  @RequestMapping("index")
-//  public String login(HttpServletRequest request, String id, String password) throws Exception {
-//    logger.info("AdminController::login() called");
-//
-//    if (request.getSession().getAttribute("admin") != null) {
-//      return "index";
-//    }
-//
-//    //Admin admin = adminService.get(id, password);
-//    if (admin != null) {
-//      HttpSession session = request.getSession();
-//      session.setAttribute("admin", admin);
-//      session.setAttribute("expire", EXPIRETIME);
-//      session.setMaxInactiveInterval(EXPIRETIME);
-//      return "index";
-//    } else {
-//      return "loginForm";
-//    }
-//  }
+  // @RequestMapping("index")
+  // public String login(HttpServletRequest request, String id, String password) throws Exception {
+  // logger.info("AdminController::login() called");
+  //
+  // if (request.getSession().getAttribute("admin") != null) {
+  // return "index";
+  // }
+  //
+  // //Admin admin = adminService.get(id, password);
+  // if (admin != null) {
+  // HttpSession session = request.getSession();
+  // session.setAttribute("admin", admin);
+  // session.setAttribute("expire", EXPIRETIME);
+  // session.setMaxInactiveInterval(EXPIRETIME);
+  // return "index";
+  // } else {
+  // return "loginForm";
+  // }
+  // }
 }
