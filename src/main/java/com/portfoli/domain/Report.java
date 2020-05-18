b34f85f49478c4f869005a4311033c623d4400c6
jeoneunhye <eunhdev@gmail.com>
2020-05-18 18:16:22 +0900

    [add] 신고 DTO 추가
    src/main/java/com/portfoli/domain/Report.java 추가
    src/main/java/com/portfoli/domain/ReportCategory.java 추가
    src/main/java/com/portfoli/domain/ReportHandleCategory.java 추가
    - 신고, 신고분류, 신고처리분류 클래스 생성 후 Serializable 인터페이스 구현
    - private 필드 선언 및 getter & setter, toString(), hashCode() &
    equals(Object) 메서드 정의

---
+++ src/main/java/com/portfoli/domain/Report.java
@@ -0,0 +1,152 @@
+package com.portfoli.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+public class Report extends Board implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private int reporterNumber;
+  private int targetNumber;
+  private Member member;
+  private List<BoardAttachment> attachments;
+  private ReportCategory reportCategory;
+  private ReportHandleCategory reportHandleCategory;
+  private Date handleDate;
+  private String handleContent;
+
+  @Override
+  public String toString() {
+    return "Report [reporterNumber=" + reporterNumber + ", targetNumber=" + targetNumber
+        + ", member=" + member + ", attachments=" + attachments + ", reportCategory="
+        + reportCategory + ", reportHandleCategory=" + reportHandleCategory + ", handleDate="
+        + handleDate + ", handleContent=" + handleContent + "]";
+  }
+
+  public int getReporterNumber() {
+    return reporterNumber;
+  }
+
+  public void setReporterNumber(int reporterNumber) {
+    this.reporterNumber = reporterNumber;
+  }
+
+  public int getTargetNumber() {
+    return targetNumber;
+  }
+
+  public void setTargetNumber(int targetNumber) {
+    this.targetNumber = targetNumber;
+  }
+
+  public Member getMember() {
+    return member;
+  }
+
+  public void setMember(Member member) {
+    this.member = member;
+  }
+
+  public List<BoardAttachment> getAttachments() {
+    return attachments;
+  }
+
+  public void setAttachments(List<BoardAttachment> attachments) {
+    this.attachments = attachments;
+  }
+
+  public ReportCategory getReportCategory() {
+    return reportCategory;
+  }
+
+  public void setReportCategory(ReportCategory reportCategory) {
+    this.reportCategory = reportCategory;
+  }
+
+  public ReportHandleCategory getReportHandleCategory() {
+    return reportHandleCategory;
+  }
+
+  public void setReportHandleCategory(ReportHandleCategory reportHandleCategory) {
+    this.reportHandleCategory = reportHandleCategory;
+  }
+
+  public Date getHandleDate() {
+    return handleDate;
+  }
+
+  public void setHandleDate(Date handleDate) {
+    this.handleDate = handleDate;
+  }
+
+  public String getHandleContent() {
+    return handleContent;
+  }
+
+  public void setHandleContent(String handleContent) {
+    this.handleContent = handleContent;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((attachments == null) ? 0 : attachments.hashCode());
+    result = prime * result + ((handleContent == null) ? 0 : handleContent.hashCode());
+    result = prime * result + ((handleDate == null) ? 0 : handleDate.hashCode());
+    result = prime * result + ((member == null) ? 0 : member.hashCode());
+    result = prime * result + ((reportCategory == null) ? 0 : reportCategory.hashCode());
+    result =
+        prime * result + ((reportHandleCategory == null) ? 0 : reportHandleCategory.hashCode());
+    result = prime * result + reporterNumber;
+    result = prime * result + targetNumber;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!super.equals(obj))
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Report other = (Report) obj;
+    if (attachments == null) {
+      if (other.attachments != null)
+        return false;
+    } else if (!attachments.equals(other.attachments))
+      return false;
+    if (handleContent == null) {
+      if (other.handleContent != null)
+        return false;
+    } else if (!handleContent.equals(other.handleContent))
+      return false;
+    if (handleDate == null) {
+      if (other.handleDate != null)
+        return false;
+    } else if (!handleDate.equals(other.handleDate))
+      return false;
+    if (member == null) {
+      if (other.member != null)
+        return false;
+    } else if (!member.equals(other.member))
+      return false;
+    if (reportCategory == null) {
+      if (other.reportCategory != null)
+        return false;
+    } else if (!reportCategory.equals(other.reportCategory))
+      return false;
+    if (reportHandleCategory == null) {
+      if (other.reportHandleCategory != null)
+        return false;
+    } else if (!reportHandleCategory.equals(other.reportHandleCategory))
+      return false;
+    if (reporterNumber != other.reporterNumber)
+      return false;
+    if (targetNumber != other.targetNumber)
+      return false;
+    return true;
+  }
+}
