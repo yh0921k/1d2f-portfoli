23c78abe2e053207380fa2bf8415c88737f2cf94
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-05-28 16:30:45 +0900

    [mod] Skill 도메인 수정
    - 수정
      - Domain : Skill
      - Mapper : PortfolioSkillMapper

--- src/main/java/com/portfoli/domain/Skill.java
+++ src/main/java/com/portfoli/domain/Skill.java
@@ -4,34 +4,41 @@ import java.io.Serializable;
 
 public class Skill implements Serializable {
   private static final long serialVersionUID = 1L;
-  int number;
-  String name;
+  int number; // 기술번호
+  int fieldNumber; // 분야번호
+  String name; // 기술이름
 
   public int getNumber() {
     return number;
   }
-
-  public void setNumber(int number) {
+  public Skill setNumber(int number) {
     this.number = number;
+    return this;
   }
-
   public String getName() {
     return name;
   }
-
-  public void setName(String name) {
+  public Skill setName(String name) {
     this.name = name;
+    return this;
+  }
+  public int getFieldNumber() {
+    return fieldNumber;
+  }
+  public Skill setFieldNumber(int fieldNumber) {
+    this.fieldNumber = fieldNumber;
+    return this;
   }
 
   @Override
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + fieldNumber;
+    result = prime * result + (name == null ? 0 : name.hashCode());
     result = prime * result + number;
     return result;
   }
-
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -44,6 +51,9 @@ public class Skill implements Serializable {
       return false;
     }
     Skill other = (Skill) obj;
+    if (fieldNumber != other.fieldNumber) {
+      return false;
+    }
     if (name == null) {
       if (other.name != null) {
         return false;
@@ -56,10 +66,10 @@ public class Skill implements Serializable {
     }
     return true;
   }
-
   @Override
   public String toString() {
-    return "Skill [number=" + number + ", name=" + name + "]";
+    return "Skill [number=" + number + ", fieldNumber=" + fieldNumber + ", name=" + name + "]";
   }
+
 
 }
