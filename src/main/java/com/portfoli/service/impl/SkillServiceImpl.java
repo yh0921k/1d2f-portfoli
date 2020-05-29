50a34fdf8d4ab718e46cfe37165dc8cef2910716
YongHwi Kim <yh0921k@gmail.com>
2020-05-29 10:19:33 +0900

    [add] 기술 관련 Service 인터페이스 및 구현 클래스 추가
    - list() 메서드는 전체 기술 리스트를 반환
    - list(fieldNumber) 메서드는 해당 분야의 기술 리스트를 반환

---
+++ src/main/java/com/portfoli/service/impl/SkillServiceImpl.java
@@ -0,0 +1,32 @@
+package com.portfoli.service.impl;
+
+import java.util.List;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+import com.portfoli.dao.SkillDao;
+import com.portfoli.domain.Skill;
+import com.portfoli.service.SkillService;
+
+@Component
+public class SkillServiceImpl implements SkillService {
+
+  TransactionTemplate transactionTemplate;
+  SkillDao skillDao;
+
+  public SkillServiceImpl(//
+      PlatformTransactionManager txManager, SkillDao skillDao) {
+    this.transactionTemplate = new TransactionTemplate(txManager);
+    this.skillDao = skillDao;
+  }
+
+  @Override
+  public List<Skill> list() throws Exception {
+    return skillDao.findAll();
+  }
+
+  @Override
+  public List<Skill> list(int fieldNumber) throws Exception {
+    return skillDao.findAllByNumber(fieldNumber);
+  }
+}
