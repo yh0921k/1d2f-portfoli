a4c545fc7d7034b6461af7b17f9779d9524cafcc
Yonghwi Kim <yh0921k@gmail.com>
2020-05-30 19:13:13 +0900

    [mod] 기술 데이터 관련 add, delete, get 기능 처리를 위한 DAO 등 구현
    - delete()
      : map으로 스킬 번호와 회원 번호를 전달받아 사용자 기술 데이터 삭제
    
    - insert()
      : map으로 스킬 번호와 회원 번호를 전달받아 사용자 기술 데이터 추가
    
    - findByName()
      : string으로 스킬 이름을 전달받아 해당 스킬 데이터 상세 조회
      : 현재 다른 필드에 같은 스킬이 여러개 존재하여 limit 1 처리
    
    - findMemberSkill()
      : map으로 사용자 번호와 스킬 이름을 전달받아 MemberSkill 객체 반환
      : pf_member_skill과 pf_skill 테이블을 스킬 번호로 join
      : 이후 pf_member_skill의 해당 사용자 번호로 필터링
      : 이후 pf_skill의 스킬 이름으로 필터링

--- src/main/java/com/portfoli/dao/SkillDao.java
+++ src/main/java/com/portfoli/dao/SkillDao.java
@@ -1,6 +1,8 @@
 package com.portfoli.dao;
 
 import java.util.List;
+import java.util.Map;
+import com.portfoli.domain.GeneralMemberSkill;
 import com.portfoli.domain.Skill;
 
 public interface SkillDao {
@@ -10,4 +12,12 @@ public interface SkillDao {
   public List<Skill> findAllByNumber(int fieldNumber) throws Exception;
 
   public List<Skill> findAllByMemberNumber(int memberNumber) throws Exception;
+
+  public int delete(Map<String, Object> params) throws Exception;
+
+  public int insert(Map<String, Object> params) throws Exception;
+
+  public Skill findByName(String skillName) throws Exception;
+
+  public GeneralMemberSkill findMemberSkill(Map<String, Object> params) throws Exception;
 }
