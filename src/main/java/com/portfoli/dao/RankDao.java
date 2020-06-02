0f95342daed80c9f0e092e2372107d09ac5edd37
YongHwi Kim <yh0921k@gmail.com>
2020-06-02 17:16:48 +0900

    [mod] 랭킹 페이지 필터링 구현 중 DAO 및 Mapper 변경
    - 사용자 필터링 요청 파라미터에 대한 findAllByFilter 구현
    - 스킬 리스트와 정렬 순서를 mybatis로 제어

--- src/main/java/com/portfoli/dao/RankDao.java
+++ src/main/java/com/portfoli/dao/RankDao.java
@@ -1,9 +1,12 @@
 package com.portfoli.dao;
 
 import java.util.List;
+import java.util.Map;
 import com.portfoli.domain.Rank;
 
 public interface RankDao {
 
   public List<Rank> findAll() throws Exception;
+
+  public List<Rank> findAllByFilter(Map<String, Object> params) throws Exception;
 }
