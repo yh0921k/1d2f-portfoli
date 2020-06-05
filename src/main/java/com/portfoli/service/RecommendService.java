ef5b09e6a4716671621c510ffa0657028f136435
YongHwi Kim <yh0921k@gmail.com>
2020-06-05 18:03:45 +0900

    [add] 인재추천정보 페이지 필터링을 위한 Service 및 구현 클래스 추가
    list()
    - 선택된 필터 요소가 없을때 전체 리스트 출력
    
    list(Map<String, Object> params)
    - 필터 요소를 map에서 추출해 해당 필터링을 거친 후 리스트 출력

---
+++ src/main/java/com/portfoli/service/RecommendService.java
@@ -0,0 +1,12 @@
+package com.portfoli.service;
+
+import java.util.List;
+import java.util.Map;
+import com.portfoli.domain.Recommend;
+
+public interface RecommendService {
+
+  public List<Recommend> list() throws Exception;
+
+  public List<Recommend> list(Map<String, Object> params) throws Exception;
+}
