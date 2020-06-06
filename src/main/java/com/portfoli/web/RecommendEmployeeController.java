56d1c0f68887b439f243f1f9476d87fe4c156761
Yonghwi Kim <yh0921k@gmail.com>
2020-06-06 23:27:24 +0900

    [mod] 인재추천정보 컨트롤러에서 필터링 처리할 핸들러 추가
    - 현재 사용자 요청 필터링 항목들을 JSON으로 받아와 출력 가능

--- src/main/java/com/portfoli/web/RecommendEmployeeController.java
+++ src/main/java/com/portfoli/web/RecommendEmployeeController.java
@@ -1,14 +1,19 @@
 package com.portfoli.web;
 
 import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.portfoli.domain.City;
 import com.portfoli.domain.District;
 import com.portfoli.domain.Field;
@@ -69,4 +74,46 @@ public class RecommendEmployeeController {
     model.addAttribute("fields", fields);
   }
 
+  @PostMapping(value = "listByFilter", produces = "text/plain;charset=UTF-8")
+  @ResponseBody
+  public String listByFilter(HttpServletRequest request,
+      @RequestBody Map<String, Object> convertedData) throws Exception {
+
+    String careerNew = (String) convertedData.get("careerNew");
+    String careerStart = (String) convertedData.get("careerStart");
+    String careerEnd = (String) convertedData.get("careerEnd");
+    String education = (String) convertedData.get("selectEducation");
+    String viewOrder = (String) convertedData.get("viewOrder");
+    List<Skill> skillList = (List<Skill>) convertedData.get("skillList");
+    List<District> districtList = (List<District>) convertedData.get("districtList");
+
+    System.out.println(careerNew);
+    System.out.println(careerStart);
+    System.out.println(careerEnd);
+    System.out.println(education);
+    System.out.println(viewOrder);
+    System.out.println(skillList);
+    System.out.println(districtList);
+    // List<Rank> rankList = rankService.list(convertedData);
+    // if (skillList.size() > 0) {
+    // rankList.sort((o1, o2) -> {
+    // int size = o1.getSkill().size();
+    // int compareSize = o2.getSkill().size();
+    //
+    // if (size < compareSize) {
+    // return 1;
+    // } else if (size > compareSize) {
+    // return -1;
+    // }
+    // return 0;
+    // });
+    // }
+    //
+    // request.setAttribute("rankList", rankList);
+    // for (Rank r : rankList) {
+    // System.out.println(r);
+    // }
+    // return new Gson().toJson(rankList);
+    return "OK";
+  }
 }
