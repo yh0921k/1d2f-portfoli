a56adceb1fb6965b9244a61f4f1699d34c984380
Yonghwi Kim <yh0921k@gmail.com>
2020-06-07 10:12:31 +0900

    [mod] 인재추천 페이지 처리할 컨트롤러 수정
    - 다중 collection 문제 발생으로 중복 데이터 하드코딩 처리중

--- src/main/java/com/portfoli/web/RecommendEmployeeController.java
+++ src/main/java/com/portfoli/web/RecommendEmployeeController.java
@@ -1,5 +1,6 @@
 package com.portfoli.web;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
@@ -14,17 +15,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.google.gson.Gson;
 import com.portfoli.domain.City;
 import com.portfoli.domain.District;
 import com.portfoli.domain.Field;
 import com.portfoli.domain.FinalEducation;
 import com.portfoli.domain.GeneralMember;
+import com.portfoli.domain.Recommend;
 import com.portfoli.domain.Skill;
 import com.portfoli.service.CityService;
 import com.portfoli.service.DistrictService;
 import com.portfoli.service.FieldService;
 import com.portfoli.service.FinalEducationService;
 import com.portfoli.service.MemberService;
+import com.portfoli.service.RecommendService;
 import com.portfoli.service.SkillService;
 
 
@@ -57,6 +61,9 @@ public class RecommendEmployeeController {
   @Autowired
   FieldService fieldService;
 
+  @Autowired
+  RecommendService recommendService;
+
   @GetMapping("recommend")
   public void recommendEmployee(Model model, @RequestParam(defaultValue = "109000") int cityNumber,
       @RequestParam(defaultValue = "40101") int fieldNumber) throws Exception {
@@ -87,33 +94,26 @@ public class RecommendEmployeeController {
     List<Skill> skillList = (List<Skill>) convertedData.get("skillList");
     List<District> districtList = (List<District>) convertedData.get("districtList");
 
-    System.out.println(careerNew);
-    System.out.println(careerStart);
-    System.out.println(careerEnd);
-    System.out.println(education);
-    System.out.println(viewOrder);
-    System.out.println(skillList);
-    System.out.println(districtList);
-    // List<Rank> rankList = rankService.list(convertedData);
-    // if (skillList.size() > 0) {
-    // rankList.sort((o1, o2) -> {
-    // int size = o1.getSkill().size();
-    // int compareSize = o2.getSkill().size();
-    //
-    // if (size < compareSize) {
-    // return 1;
-    // } else if (size > compareSize) {
-    // return -1;
-    // }
-    // return 0;
-    // });
-    // }
-    //
-    // request.setAttribute("rankList", rankList);
-    // for (Rank r : rankList) {
-    // System.out.println(r);
-    // }
-    // return new Gson().toJson(rankList);
-    return "OK";
+    if (careerStart.length() > 0) {
+      convertedData.put("careerStart",
+          Integer.parseInt(((String) convertedData.get("careerStart")).substring(0, 1)));
+    }
+
+    if (careerEnd.length() > 0) {
+      convertedData.put("careerEnd",
+          Integer.parseInt(((String) convertedData.get("careerEnd")).substring(0, 1)));
+    }
+
+    List<Recommend> rankList = recommendService.list(convertedData);
+    List<Recommend> result = new ArrayList<>();
+    for (int i = 0; i < rankList.size(); i++) {
+
+    }
+
+    for (Recommend r : rankList) {
+      System.out.println(r);
+    }
+
+    return new Gson().toJson(rankList);
   }
 }
